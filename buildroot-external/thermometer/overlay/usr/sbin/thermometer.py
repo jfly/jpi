f4459decc9f327dc74cedac955f8bf5c9a71a73c
Jeremy Fleischman <jeremyfleischman@gmail.com>
2020-02-26 09:11:55 -0800

    Implementing temperature api.

--- buildroot-external/thermometer/overlay/usr/sbin/thermometer.py
+++ buildroot-external/thermometer/overlay/usr/sbin/thermometer.py
@@ -1,36 +1,62 @@
 #!/usr/bin/micropython
 
+import glob
+import json
+import os.path
+import socket
+
 # Copied (and modified) from
 # https://docs.micropython.org/en/latest/esp8266/tutorial/network_tcp.html#simple-http-server
 
-html = """<!DOCTYPE html>
-<html>
-    <head> <title>ESP8266 Pins</title> </head>
-    <body> <h1>ESP8266 Pins</h1>
-        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
-    </body>
-</html>
-"""
+def get_temperatures_celsius():
+    temperature_by_name = {}
+    for temperature_file in glob.glob("/sys/class/hwmon/hwmon*/temp1_input"):
+        device_name_file = os.path.join(os.path.dirname(temperature_file), "device/name")
+        with open(temperature_file, "r") as f_temp, open(device_name_file, "r") as f_name:
+            temperature_str = f_temp.read().strip()
+            name = f_name.read().strip()
+        temperature_celsius = int(temperature_str)/1000
+        temperature_by_name[name] = temperature_celsius
 
-import socket
-addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    return temperature_by_name
 
-s = socket.socket()
-s.bind(addr)
-s.listen(1)
+def c_to_f(c):
+    return c * (9/5) + 32
 
-print('listening on', addr)
+def handle_request():
+    data = {
+        name: {
+            "celsius": celsius,
+            "fahrenheit": c_to_f(celsius),
+        } for name, celsius in get_temperatures_celsius().items()
+    }
+    return data, "application/json"
 
-while True:
-    cl, addr = s.accept()
-    print('client connected from', addr)
-    cl_file = cl.makefile('rwb', 0)
+def main():
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #<<<
+    s.bind(addr)
+    s.listen(1)
+
+    print('listening on', addr)
+
     while True:
-        line = cl_file.readline()
-        if not line or line == b'\r\n':
-            break
-    rows = ['<tr><td>%s</td><td>%s</td></tr>' % ("hiya", "biya")]
-    response = html % '\n'.join(rows)
-    cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-    cl.send(response)
-    cl.close()
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+
+        response, content_type = handle_request()
+        if content_type == "application/json":
+            response = json.dumps(response)
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: %s\r\n\r\n' % content_type)
+        cl.send(response)
+        cl.close()
+
+if __name__ == "__main__":
+    main()
